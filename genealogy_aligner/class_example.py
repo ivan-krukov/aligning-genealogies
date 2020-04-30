0c40b1d87a229e765198986dbe9b5e56c1ec74cf
ivan-krukov <ivan.o.kryukov@gmail.com>
2020-04-30 19:21:24 -0400

    An example script showing the graphs that the classes implement

---
+++ genealogy_aligner/class_example.py
@@ -0,0 +1,23 @@
+from Genealogy import Genealogy
+import matplotlib.pyplot as plt
+
+families = 5
+generations = 7
+avg_children = 2.2
+G = Genealogy.from_founders(families, generations, avg_children)
+while G.generations < generations:
+    # if a simulation terminated early (everyone died)
+    print('retrying')
+    G = Genealogy.from_founders(families, generations, avg_children)
+    
+T = G.sample_tree()
+
+fig, ax = plt.subplots(ncols=2, figsize=(20, 10))
+
+G.draw(ax=ax[0])
+ax[0].set_title('Genealogy')
+T.draw(ax=ax[1])
+ax[1].set_title('Coalescent traversal')
+
+fig.savefig('fig/class_example.png', dpi=300)
+
