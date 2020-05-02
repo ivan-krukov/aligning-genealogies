05e787896bdd34802e256cfcced00d1acb862cec
Shadi <shadizabad1@gmail.com>
2020-05-02 17:55:18 -0400

    Modifying similarity matrix scoring to remove known probands.

--- genealogy_aligner/greedy_alignment.py
+++ genealogy_aligner/greedy_alignment.py
@@ -1,19 +1,25 @@
 import numpy as np
 
 
-def create_similarity_matrix(gg, ct):
+def create_similarity_matrix(gg, ct, climb_up_step=0):
 
     gg_nodes = list(gg.nodes())
     ct_nodes = list(ct.nodes())
 
-    gg_ntp = gg.get_probands_under()
-    ct_ntp = ct.get_probands_under()
+    gg_ntp = gg.get_probands_under(climb_up_step=climb_up_step)
+    ct_ntp = ct.get_probands_under(climb_up_step=climb_up_step)
+
+    gg_prob = gg.probands(use_time=False)
+    ct_prob = ct.probands(use_time=False)
 
     pairs = []
     scores = []
 
     for i, n_gg in enumerate(gg_nodes):
         for j, n_ct in enumerate(ct_nodes):
+
+            if n_gg in gg_prob or n_ct in ct_prob:
+                continue
 
             score = (len(gg_ntp[n_gg].intersection(ct_ntp[n_ct])) /
                      len(gg_ntp[n_gg].union(ct_ntp[n_ct])))
